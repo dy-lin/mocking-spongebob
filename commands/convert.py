be688b6d571754437c4596061c7af9d0a6bc529b
Diana Lin <diana.lin@alumni.ubc.ca>
2022-12-18 12:45:27 -0800

    add support for celsius

--- commands/convert.py
+++ commands/convert.py
@@ -17,7 +17,7 @@ class Convert(BaseCommand):
         # Parameters will be separated by spaces and fed to the 'params' 
         # argument in the handle() method
         # If no params are expected, leave this list empty or set it to None
-        params = ["temperature (F)", "time (min)"]
+        params = ["temperature", "time (min)"]
         super().__init__(description, params)
 
     # Override the handle() method
@@ -25,12 +25,29 @@ class Convert(BaseCommand):
     async def handle(self, params, message, client):
         # if the temperature contains F, remove the F and do the math
         degree_sign = u'\N{DEGREE SIGN}'
-        temp_F = str(params[0]).upper()
-        temp = int(temp_F.replace("F", ""))
-        final_temp = f"{temp - 25}{degree_sign}F"
+        temperature = str(params[0].upper())
+        if temperature.endswith("F") == True:
+            fah = True
+            temp_F = int(temperature.replace("F", ""))
+            final_temp_F = f"{temp_F - 25}{degree_sign}F"
+            temp_C = round((temp_F-25-32)*(5/9))
+            final_temp_C = f"{temp_C}{degree_sign}C"
+        elif temperature.endswith("C") == True:
+            fah = False
+            temp_C = int(temperature.replace("C", ""))
+            temp_F = round((temp_C*9/5)+32)
+            temp_C = round((temp_F-25-32)*(5/9))
+            final_temp_F = f"{temp_F - 25}{degree_sign}F"
+            final_temp_C = f"{temp_C}{degree_sign}C"
+        else:
+            # assume fahrenheit
+            fah = True
+            temp_F = int(temperature)
+            final_temp_F = f"{temp_F - 25}{degree_sign}F"
+            temp_C = round((temp_F-25-32)*(5/9))
+            final_temp_C = f"{temp_C}{degree_sign}C"
 
         time = str(params[1]).lower().split("-")
-        
         times = []
         for bound in time:
             num = int(re.findall('[0-9]+', bound)[0])
@@ -43,6 +60,9 @@ class Convert(BaseCommand):
                 final_time = f"{times[0]} min"
             else:
                 final_time = f"{times[0]}-{times[1]} min"
-        
-        msg = f"Airfry at {final_temp} for {final_time}"
+
+        if fah == True: 
+            msg = f"Airfry at {final_temp_F} ({final_temp_C}) for {final_time}"
+        else:
+            msg = f"Airfry at {final_temp_C} ({final_temp_F}) for {final_time}"
         await message.channel.send(msg)
