8c793211bdd53b231cebaee3ed99b6d58a53a9b2
Diana Lin <diana.lin@alumni.ubc.ca>
2024-02-10 15:34:54 -0800

    Add command to scrape connections page for hints

---
+++ commands/connections.py
@@ -0,0 +1,37 @@
+from commands.base_command  import BaseCommand
+import subprocess
+# Your friendly example event
+# Keep in mind that the command name will be derived from the class name
+# but in lowercase
+# So, a command class named Random will generate a 'random' command
+class Conn(BaseCommand):
+
+    def __init__(self):
+        # A quick description for the help message
+        description = "Provides hints to the NYT Connections"
+        # A list of parameters that the command will take as input
+        # Parameters will be separated by spaces and fed to the 'params' 
+        # argument in the handle() method
+        # If no params are expected, leave this list empty or set it to None
+        params = []
+        super().__init__(description, params)
+
+    # Override the handle() method
+    # It will be called every time the command is received
+    async def handle(self, params, message, client):
+        # 'params' is a list that contains the parameters that the command 
+        # expects to receive, t is guaranteed to have AT LEAST as many
+        # parameters as specified in __init__
+        # 'message' is the discord.py Message object for the command to handle
+        # 'client' is the bot Client object
+
+        cxn = subprocess.getoutput(['/Users/dianalin/mocking-spongebob/helpers/download_connections.sh']).split("\n\n")
+
+        if cxn == 'NULL':
+            msg = f"Today's date does not match the Connections date."
+            await message.channel.send(msg)
+        else:
+            print(cxn)
+            for i in cxn:
+                await message.channel.send(i)
+
