e79596ed5af05ee81447ec36c3da93793d424225
Diana Lin <diana.lin@alumni.ubc.ca>
2024-02-13 18:22:49 -0800

    remove empty lines in hints

--- helpers/download_connections.sh
+++ helpers/download_connections.sh
@@ -17,9 +17,9 @@ fi
 today=$(date '+%B-%-d-%Y')
 
 # grab panagrams
-hints=$(grep -w try $html| awk -F "try:" '{print $2}' | gsed 's|</\?[0-9a-z=" :-]\+>||g' | gsed 's/Yellow: /**Yellow:** ||/' | gsed 's/Green: /\n**Green:** ||/' | gsed 's/Blue: /\n**Blue:** ||/' | gsed 's/Purple: /\n**Purple:** ||/' | gsed 's/$/||/' | gsed 's/^/- /')
+hints=$(grep -w try $html| awk -F "try:" '{print $2}' | gsed 's|</\?[0-9a-z=" :-]\+>||g' | gsed 's/Yellow: /**Yellow:** ||/' | gsed 's/Green: /\n**Green:** ||/' | gsed 's/Blue: /\n**Blue:** ||/' | gsed 's/Purple: /\n**Purple:** ||/' | gsed '/^$/d' | gsed 's/$/||/' | gsed 's/^/- /')
 categories=$(grep 'extra help' $html | awk -F "categories:" '{print $2}' | gsed 's|</\?[a-oq-z0-9]\+>||g' | awk -F "Looking for Wordle"  '{print $1}' |gsed 's|</p><p>|\n|g' | gsed '/^$/d' | gsed 's/: /:** ||/' | gsed 's/^/- **/' | gsed 's/$/||/')
-soln=$(grep 'What is the answer to' $html | grep -o '</strong>: [A-Z, ]\+</p>' | gsed 's|</\?[a-z]\+>:\? \?||g' | gsed '1s/^/**Yellow:** ||/' | gsed '2s/^/**Green:** ||/' | gsed '3s/^/**Blue:** ||/' | gsed '4s/^/**Purple:** ||/' | gsed 's/, /||   ||/g' | gsed 's/$/||/' | gsed 's/^/- /')
+soln=$(grep 'What is the answer to' $html | grep -o '</strong>: [A-Z, ]\+</p>' | gsed 's|</\?[a-z]\+>:\? \?||g' | gsed '1s/^/**Yellow:** ||/' | gsed '2s/^/**Green:** ||/' | gsed '3s/^/**Blue:** ||/' | gsed '4s/^/**Purple:** ||/' | gsed 's/, /||   ||/g' | gsed '/^$/d' | gsed 's/$/||/' | gsed 's/^/- /')
 
 if [[ "$today" != "$url_date" ]]; then
 	echo "NULL"
