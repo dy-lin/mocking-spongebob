8751cb00331ae08e453f6bbd73893d3b0d8c54e9
Diana Lin <diana.lin@alumni.ubc.ca>
2024-05-04 11:35:37 -0700

    change regex due to ads

--- helpers/download_connections.sh
+++ helpers/download_connections.sh
@@ -34,7 +34,7 @@ else
 fi
 
 # grab panagrams
-hints=$(grep -w try $html| awk -F "try:" '{print $2}' | gsed 's|</\?[0-9a-z=" :-]\+>||g' | gsed 's/Yellow: /**Yellow:** ||/' | gsed 's/Green: /\n**Green:** ||/' | gsed 's/Blue: /\n**Blue:** ||/' | gsed 's/Purple: /\n**Purple:** ||/' | gsed '/^$/d' | gsed 's/$/||/' | gsed 's/^/- /')
+hints=$(grep -m1 'Yellow:' $html | awk -F "<strong>|</strong>" 'BEGIN{OFS="\n"} {print $2, $4, $6, $8}' | gsed 's|</\?[0-9a-z=" :-]\+>||g' | gsed '1s/^/**Yellow:** ||/' | gsed '2s/^/**Green:** ||/' | gsed '3s/^/**Blue:** ||/' | gsed '4s/^/**Purple:** ||/' | gsed 's/, /||   ||/g' | gsed '/^$/d' | gsed 's/$/||/' | gsed 's/^/- /')
 categories=$(grep 'extra help' $html | awk -F "categories:" '{print $2}' | gsed 's|</\?[a-oq-z0-9]\+>||g' | awk -F "Looking for Wordle"  '{print $1}' |gsed 's|</p><p>|\n|g' | gsed '/^$/d' | gsed 's/: /:** ||/' | gsed 's/^/- **/' | gsed 's/$/||/')
 soln=$(grep 'What is the answer to' $html | grep -o '</strong>:* *[A-Z, ()]\+</p>' | gsed 's|</\?[a-z]\+>:\? \?||g' | gsed '1s/^/**Yellow:** ||/' | gsed '2s/^/**Green:** ||/' | gsed '3s/^/**Blue:** ||/' | gsed '4s/^/**Purple:** ||/' | gsed 's/, /||   ||/g' | gsed '/^$/d' | gsed 's/$/||/' | gsed 's/^/- /')
 echo "# Hints:"
