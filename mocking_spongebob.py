44c9328820f018e53500673495f5512dcd7862f7
Diana Lin <diana.lin@alumni.ubc.ca>
2024-10-21 08:54:49 -0700

    update main script

--- mocking_spongebob.py
+++ mocking_spongebob.py
@@ -79,6 +79,15 @@ def main():
                     print("Error while handling message", error, flush=True)
                     print("Use `!embed` `dd|ez|current|off`", flush = True)
                     raise
+            elif "tiktok.com" in text:
+                try:
+                    await message_handler.handle_command("tt", 
+                                          text.split(), message, client)
+                except Exception as error:
+                    print("Error while handling message", error, flush=True)
+                    raise
+
+                    
         else:
             print(f"{message.author.name}: {text}", flush = True)
 
